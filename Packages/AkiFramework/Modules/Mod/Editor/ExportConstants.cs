21947d37e35f9adad909fb630f244bb091abef6d
AkiKurisu <2683987717@qq.com>
2025-01-18 19:23:28 +0800

    Exclude no need module

--- Packages/AkiFramework/Modules/Mod/Editor/ExportConstants.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.IO;
-namespace Kurisu.Framework.Mod.Editor
-{
-    public class ExportConstants
-    {
-        public static string ExportPath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "Export");
-    }
-}
